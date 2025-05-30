deba2fd0d0a0dc81cd0a547038b2206bab9031ee
JnsRdnp <112471004+JnsRdnp@users.noreply.github.com>
2025-05-30 13:09:22 +0300

    Before generating all models

---
+++ scripts/generateModels.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const { exec } = require('child_process');
+
+function generateModels() {
+  const {
+    MYSQL_HOST,
+    DB_PORT=3306,
+    MYSQL_DATABASE,
+    MYSQL_USER,
+    MYSQL_PASSWORD,
+  } = process.env;
+
+  const command = `npx sequelize-auto -h ${MYSQL_HOST} -d ${MYSQL_DATABASE} -u ${MYSQL_USER} -x ${MYSQL_PASSWORD} -p ${DB_PORT} -e postgres -o ./models`;
+
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`❌ Error generating models: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.error(`⚠️ stderr: ${stderr}`);
+      return;
+    }
+    console.log(`✅ Models generated successfully:\n${stdout}`);
+  });
+}
+
+generateModels();
