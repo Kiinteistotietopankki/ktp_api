a0b6f121081f428a36a06942f1e9f643d22e65f2
JnsRdnp <112471004+JnsRdnp@users.noreply.github.com>
2025-06-16 15:22:35 +0300

    cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const sequelize = require('./config/dbConfig');
 require('dotenv').config();
 
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:3000' })); 
+// app.use(cors({ origin: 'http://localhost:3000' })); 
 
 
 app.use('/api/kiinteistot', kiinteistoRoutes)
